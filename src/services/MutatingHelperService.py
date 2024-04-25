2ae9e94bd3392cd96faba1fb54ecda86ccb04f04
Ozan UYSAL <ozan.uysal@msn.com>
2024-04-25 15:18:06 +0300

    notebook injecting secrerts with replace command

--- src/services/MutatingHelperService.py
+++ src/services/MutatingHelperService.py
@@ -7,7 +7,7 @@ class MutatingHelperService:
         pass
 
     def add_secret_for_notebook(self, key_name : str, real_value : str):
-        return {"op": "add", "path": "/spec/template/spec/containers/0/env/-", "value": {"name": key_name, "value": real_value}}
+        return {"op": "replace", "path": "/spec/template/spec/containers/0/env/-", "value": {"name": key_name, "value": real_value}}
     
     def add_secret_for_pipeline(self, task_index : int, key_name : str, real_value : str):
         return {"op": "add", "path": f"/spec/pipelineSpec/tasks/{task_index}/taskSpec/steps/0/env/-", "value": {"name": key_name, "value": real_value}}
